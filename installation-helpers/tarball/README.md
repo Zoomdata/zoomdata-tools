1b834f6d4615ee51fb32158330b42ca1a5796f0f
Justin Boyd <boyd@zoomdata.com>
2016-11-10 13:02:55 -0500

    Initial commit for installation-helpers, tarball deployment script, and readme

---
+++ installation-helpers/tarball/README.md
@@ -0,0 +1,33 @@
+# Copyright (C) Zoomdata, Inc. 2012-2016. All rights reserved.
+
+Sample Bash script for deploying Zoomdata from tarball packaging. This script is most useful for situations where the user deploying Zoomdata does not have root or sudo access on the host OS.
+
+## Prerequisites:
+* Zoomdata components in tarball packaging (provided by your Zoomdata representative)
+* Java 1.8 installed or available via `$JAVA_HOME` on the Zoomdata host
+* Max open file limit adjusted for the OS user running the Zoomdata processes: http://docs.zoomdata.com/installation-prerequisites
+* An accessible Postgresql 9.5 instance with a Zoomdata user account and databases setup: http://docs.zoomdata.com/install-and-set-up-zoomdata-metadata-store
+* [OPTIONAL] Install Firefox to enable screenshotting of Zoomdata dashboards/visualizations: http://docs.zoomdata.com/post-installation-options
+
+## Overview
+This helper script
+
+1. creates a `./zoomdata` directory within the `INSTALL_DIR`
+1. unpacks all Zoomdata components to the `./zoomdata` directory
+1. starts all Zoomdata component processes in the background
+
+## Usage
+
+1. Copy the Zoomdata tarballs and [the helper script](zoomdata-tarball-deployment.sh) to the same directory on the host
+1. Edit the following variables in the helper script according to the environment
+
+    | Variable | Description | Default |
+    | ------------- | ------------- | ------------- |
+    | PG_HOST | Postgres host | localhost |
+    | PG_PORT | Postgres port | 5432 |
+    | PG_USER | Postgres connection user name | zoomdata |
+    | PG_PASSWORD | Postgres connection user password | password |
+    | PG_ZOOMDATA_DB | Zoomdata metadata schema in Postgres| zoomdata |
+    | PG_SCHEDULER_DB | Zoomdata-scheduler metadata schema in Postgres| zoomdata-scheduler |
+1. Execute the script - `./zoomdata-tarball-deployment.sh`
+
---
+++ installation-helpers/tarball/zoomdata-tarball-deployment.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+################################################################################
+## ZOOMDATA 2.3 TARBALL DEPLOYMENT ##
+# Prerequisites:
+# - Java 1.8 installed on host
+# - Max open file limit has been set for user: http://docs.zoomdata.com/installation-prerequisites
+# - Setup the Zoomdata account and databases in Postgres: http://docs.zoomdata.com/install-and-set-up-zoomdata-metadata-store
+# - [OPTIONAL] Install Firefox to enable screenshotting of Zoomdata dashboards/visualizations: http://docs.zoomdata.com/post-installation-options
+
+# Begin
+# Set script variables here for convenience
+INSTALL_DIR=${INSTALL_DIR:-"$(dirname $0)"}  # Installation directory of Zoomdata; place all Zoomdata related tarballs here this script should be placed here as well
+PG_HOST=${PG_HOST:-"localhost"}  # PostgreSQL Host
+PG_PORT=${PG_PORT:-"5432"}  # PostgreSQL Port
+PG_USER=${PG_USER:-"zoomdata"}  # PostgreSQL connection user name
+PG_PASSWORD=${PG_PASSWORD:-"password"}  # PostgreSQL connection user password
+PG_ZOOMDATA_DB=${PG_ZOOMDATA_DB:-"zoomdata"}  # Zoomdata metadata schema in PostgreSQL
+PG_SCHEDULER_DB=${PG_SCHEDULER_DB:-"zoomdata-scheduler"}  # Zoomdata-scheduler metadata schema in PostgreSQL
+ZOOMDATA_CONF=${INSTALL_DIR}/zoomdata/conf/zoomdata.properties
+SCHEDULER_CONF=${INSTALL_DIR}/zoomdata/conf/scheduler.properties
+
+pushd $INSTALL_DIR
+
+# Create folder for zoomdata files
+mkdir zoomdata
+
+# Unpack tarballs
+for tarball in zoomdata*.tar.gz
+do
+    tar -xzf $tarball --strip-components=1 -C zoomdata
+done
+
+# Set start scripts to be executable
+chmod +x $INSTALL_DIR/zoomdata/bin/*
+
+#Set Zoomdata Postgres configuration
+printf "spring.datasource.url=jdbc:postgresql://$PG_HOST:$PG_PORT/$PG_ZOOMDATA_DB\nspring.datasource.username=$PG_USER\nspring.datasource.password=$PG_PASSWORD\n" >> $ZOOMDATA_CONF
+
+#Set Zoomdata-Scheduler Postgres configuration
+printf "spring.datasource.url=jdbc:postgresql://$PG_HOST:$PG_PORT/$PG_SCHEDULER_DB\nspring.datasource.username=$PG_USER\nspring.datasource.password=$PG_PASSWORD\n" >> $SCHEDULER_CONF
+
+# Start consul
+$INSTALL_DIR/zoomdata/bin/consul agent -client=127.0.0.1 -bind=127.0.0.1 -bootstrap -server -data-dir=$INSTALL_DIR/zoomdata/data/consul &
+
+# Start EDC services
+for edc in $INSTALL_DIR/zoomdata/bin/zoomdata-edc* ; do
+    $edc &
+done
+
+# Start the Zoomdata-Scheduler
+$INSTALL_DIR/zoomdata/bin/zoomdata-scheduler &
+# Start the Zoomdata Spark Proxy (SparkIt)
+$INSTALL_DIR/zoomdata/bin/zoomdata-spark-proxy &
+# OPTIONAL Start the Screenshot service (XVFB)
+$INSTALL_DIR/zoomdata/bin/zoomdata-xvfb &
+# Start the Zoomdata web application
+$INSTALL_DIR/zoomdata/bin/zoomdata &
+
+popd
