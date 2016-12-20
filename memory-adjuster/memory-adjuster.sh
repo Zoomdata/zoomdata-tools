d46527bd33ced4b078f1b099d59d2bd1370c7098
Eugene Chepurniy <eugene@zoomdata.com>
2016-12-20 12:17:34 +0200

    memory-adjuster: dropped .sh script, introduced internal memory guessing mechanism, optparse introduced

--- memory-adjuster/memory-adjuster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "/etc/zoomdata" ]; then
-    mkdir -p /etc/zoomdata
-    chown zoomdata:zoomdata /etc/zoomdata
-fi
-
-/usr/bin/env python ./memory-adjuster.py `cat /proc/meminfo | grep "MemTotal" | awk '{print $2}'`
-
