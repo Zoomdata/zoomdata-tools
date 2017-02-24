103363a96888d4bda124fbaeaf3064c445f72b18
Eugene Chepurniy <eugene@zoomdata.com>
2017-02-24 10:11:16 +0200

    monitoring: updated zoomdata-metrics-collector and corresponding zabbix template

--- monitoring-templates/zabbix/zoomdata-metrics/zoomdata-metrics-collector.py
+++ monitoring-templates/zabbix/zoomdata-metrics/zoomdata-metrics-collector.py
@@ -4,19 +4,113 @@ from __future__ import print_function
 import requests
 import argparse
 import json
+import sys
 
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from os.path import getmtime
 from time import time
 from tempfile import gettempdir
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
 
 __author__ = 'eugene@zoomdata.com'
 
-DEF_TIMEOUT = 60
-DEF_CONTEXT = 'zoomdata'
-DEF_METRICS_URI = 'service/system/info/metrics'
+
 DEF_SCHEMA = 'http'
 DEF_PORT = '8080'
-DEF_TMP_FILE_FORMAT = '{0}/{1}-{2}-{3}-metrics-cache.tmp'
+DEF_CONTEXT = 'zoomdata'
+
+
+class MetricsCollector(object):
+    def __init__(self, base_url, cache_lifetime=30):
+        self.data_url = base_url
+        self.parsed_url = urlparse(self.data_url)
+        self.cache_lifetime = cache_lifetime
+        self.last_request_status = False
+        self.temp_file_name = "{tmpdir}/metrics-cache-{host}-{port}{uri}.tmp".format(
+            tmpdir = gettempdir(),
+            host = self.parsed_url.hostname,
+            port = self.parsed_url.port,
+            uri = self.parsed_url.path.replace('/', '-')
+        )
+
+    def get_last_request_status(self):
+        return self.last_request_status
+
+    def __is_cache_valid(self):
+        # check if cache file is exist
+        try:
+            mtime = getmtime(self.temp_file_name)
+        except OSError as e:
+            return False
+
+        # is this file was created within default interval?
+        if (int(time()) - mtime) > self.cache_lifetime:
+            return False
+
+        return True
+
+    def __request_data(self):
+        if self.__is_cache_valid():
+            return True
+
+        # requesting data
+        try:
+            # setting 5 sec timeouts for connect and read
+            r = requests.get(self.data_url, verify=False, allow_redirects=True, timeout=(5, 5))
+        except requests.ConnectionError as e:
+            print("Unable to connect to ", self.data_url, " error is ", e, file=sys.stderr)
+            return False
+        except requests.ConnectTimeout as e:
+            print("Timed out connection to ", self.data_url, " error is ", e, file=sys.stderr)
+            return False
+        except requests.ReadTimeout as e:
+            print("Timed out while reading data from ", self.data_url, " error is ", e, file=sys.stderr)
+            return False
+
+        if r.status_code == 200:
+            # got HTTP/200 for request - storing it in cache
+            try:
+                open(self.temp_file_name, mode="w").write(json.dumps(r.json()))
+            except IOError as e:
+                print("IO error while trying to store cache into file ", self.temp_file_name, " error is ",
+                      e, file=sys.stderr)
+                return False
+            return True
+        else:
+            return False
+
+    def get_key(self, key):
+        # mandatory call to fetch data / refresh cache
+        self.last_request_status = self.__request_data()
+        if not self.last_request_status:
+            return None
+
+        metrics = {}
+
+        try:
+            metrics = json.loads(open(self.temp_file_name, mode='r').read())
+        except IOError as e:
+            print("Error loading/parsing metrics cache file ", self.temp_file_name, " error is ", e, file=sys.stderr)
+            return None
+
+        try:
+            for key in key.split(':'):
+                # looking for '*' in key
+                if '*' in key:
+                    (key_starts_with, sep, key_ends_with) = key.rpartition('*')
+                    new_key = list(filter(lambda x: x.startswith(key_starts_with) and x.endswith(key_ends_with),
+                                          list(metrics.keys())))[0]
+                    metrics = metrics[new_key]
+                else:
+                    metrics = metrics[key]
+            return metrics
+        except:
+            #print("Key ", key, " not found in data received from ", self.data_url, file=sys.stderr)
+            return None
 
 
 def parse_args():
@@ -26,74 +120,42 @@ def parse_args():
     parser.add_argument('-s', '--schema', dest='schema', help='http/https', default=DEF_SCHEMA)
     parser.add_argument('-p', '--port', dest='port', help='tcp port to connect to', default=DEF_PORT)
     parser.add_argument('-c', '--context', dest='context', help='Zoomdata server context', default=DEF_CONTEXT)
-    parser.add_argument('-u', '--metrics_uri', dest='metrics_uri', help='Zoomdata server metrics URI', default=DEF_METRICS_URI)
 
     return parser.parse_args()
 
 
-def get_tmp_file_name(args):
-    return DEF_TMP_FILE_FORMAT.format(gettempdir(), args.host, args.context, args.metrics_uri.replace('/', '-'))
-
-
-def check_cache_valid(args):
-    # check if cache file is exist
-    try:
-        mtime = getmtime(get_tmp_file_name(args))
-    except OSError as e:
-        return False
-
-    # is this file was created within default interval?
-    if (int(time()) - mtime) > DEF_TIMEOUT:
-        return False
-
-    return True
-
-
-def request_metrics_data(args):
-    # constructing URL to query metrics from
-    url = '{0}://{1}:{2}/{3}/{4}'.format(args.schema, args.host, args.port, args.context, args.metrics_uri)
-    # proceeding with query metrics
-    try:
-        r = requests.get(url)
-    except requests.ConnectionError as e:
-        print("Unable to connect to ", url, " error is ", e, file=sys.stderr)
-        return False
-
-    if r.status_code == 200:
-        # got HTTP/200 for request - storing it in cache
-        open(get_tmp_file_name(args), mode="w").write(json.dumps(r.json()))
-    else:
-        return False
-
-    return True
-
-
-def get_metric_by_key(args):
-    # loading cached file with metrics
-    try:
-        metrics = json.loads(open(get_tmp_file_name(args), mode='r').read())
-    except IOError as e:
-        print("Error loading/parsing metrics cache file, ", e, file=sys.stderr)
-
-    try:
-        dt = metrics
-        for key in args.metric.split(':'):
-            dt = dt[key]
-        return dt
-    except IndexError as e:
-        print("Incorrect key, ", e, file=sys.stderr)
-        return None
-
-
 def main():
     args = parse_args()
-    if not check_cache_valid(args):
-        if not request_metrics_data(args):
-            print("Unable to get metrics data", file=sys.stderr)
 
-    res = get_metric_by_key(args)
-    print(res if res else '')
+    # Construct base url for zoomdata:
+    zoomdata_base_url = "{schema}://{host}:{port}/{context}".format(
+        schema=args.schema,
+        host=args.host,
+        port=args.port,
+        context=args.context
+    )
+
+    spark_base_url = "{schema}://{host}:{port}".format(
+        schema=args.schema,
+        host=args.host,
+        port=4040
+    )
+
+    data_endpoints = []
+    data_endpoints.append(MetricsCollector(zoomdata_base_url + '/service/system/info/metrics'))
+    data_endpoints.append(MetricsCollector(zoomdata_base_url + '/service/metrics_grouped'))
+    data_endpoints.append(MetricsCollector(spark_base_url + '/metrics/json'))
+
+    result = None
+    for dep in data_endpoints:
+        result = dep.get_key(args.metric)
+        if result is not None:
+            break
+
+    print(result if result is not None else '')
 
 
 if __name__ == '__main__':
+    # switch off warings for self signed certificates
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     main()
