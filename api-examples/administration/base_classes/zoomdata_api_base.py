9d0f1ace550cd3c986dc29e1caa2e4753ec7ac0e
Justin Boyd <boyd@zoomdata.com>
2017-03-03 12:58:57 -0500

    Initial commit for load_security_model API example

---
+++ api-examples/administration/base_classes/zoomdata_api_base.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import sys,urllib,urllib2,json
+
+class HttpServerRequest(object):
+  # HTTP Server request class
+  debug=False
+  debugLevel=1
+
+  def __init__(self):
+    """Return a request object """
+    self.authHeader = None
+
+  def __openrequest__(self, req):
+    # Opens the passed in HTTP request
+    if self.debug:
+      print "\n----- REQUEST -----"
+      handler = urllib2.HTTPSHandler(debuglevel=self.debugLevel)
+      opener = urllib2.build_opener(handler)
+      urllib2.install_opener(opener)
+      print "- API ENDPOINT: "+req.get_full_url()
+      print "- REQUEST METHOD: "+req.get_method()
+      print "- AUTHORIZATION HEADER: "+req.get_header("Authorization")
+      print "\n----- REQUEST DATA -----"
+      print req.get_data()
+    
+    res = urllib2.urlopen(req)
+    out = res.read()
+
+    if self.debug:
+      print "\n----- REQUEST INFO -----"
+      print res.info()
+      print "\n----- RESPONSE -----"
+      print out
+
+    return out
+
+  def submit(self,url,data=None,lmda=None,type=None):
+    # Constructs the HTTP server request
+    
+    if data != None:
+        # Add data payload to request
+        request = urllib2.Request(url, data) #PUT/POST
+        
+        if type != None:
+          # Set payload content type
+          request.add_header('Content-Type', type)
+        else:
+          # Default payload content type
+          request.add_header('Content-Type', 'application/json')
+    else:
+      # Assume GET request
+      request = urllib2.Request(url) #GET
+
+    if self.authHeader != None:
+      # Add authentication header if present
+      request.add_header('Authorization', self.authHeader)
+
+    if lmda != None:
+      # Set request method: PATCH, DELETE, etc
+      request.get_method = lambda: lmda
+
+    return self.__openrequest__(request)
+
+  def enableDebug(self, level=1):
+    # Enable verbose output of HTTP request transmission
+    self.debug=True
+    self.debugLevel=level
+  
+  def disableDebug(self):
+    # Disable verbose output of HTTP request transmission
+    self.debug=False
+
+class ZoomdataRequest(HttpServerRequest):
+  # Zoomdata HTTP Server request class
+
+  def __init__(self, zoomdataBaseURL, adminUser, adminPassword, supervisorUser=None, supervisorPassword=None):
+    """Return a Zoomdata server HTTP request object """
+    self.adminUser = adminUser
+    # Encode the admin credentials
+    self.adminAuthHeader = self.__encodeAuthHeader__(adminUser,adminPassword)
+    # Encode the supervisor credentials (if present)
+    if supervisorPassword is not None:
+      self.supervisorAuthHeader = self.__encodeAuthHeader__(supervisorUser,supervisorPassword)
+    # Initialize the Zoomdata server URL
+    self.zoomdataServer = zoomdataBaseURL
+    
+    # Default to admin authentication
+    self.useAdminAuth()
+
+  def __encodeAuthHeader__(self, user, password):
+    # Return encoded credentials to be used on the HTTP request
+    return "Basic " + (user+":"+password).encode("base64").rstrip()
+
+  def useAdminAuth(self):
+    # Authenticate to Zoomdata as an administrator (tenant-level)
+    self.authHeader = self.adminAuthHeader
+
+  def useSupervisorAuth(self):
+    # Authenticate to Zoomdata as the 'supervisor' (instance-level)
+    self.authHeader = self.supervisorAuthHeader
+
+  def getCurrentAuthUser(self):
+    # Return current auth configuration
+    if self.authHeader == self.adminAuthHeader:
+      return self.adminUser
+    else:
+      return "supervisor"
+
+class ZoomdataObject(object):
+  # Base Zoomdata metadata + server request object
+  def __init__(self, name, serverRequest):
+    self.id = ""
+    self.name = name
+    self.serverRequest = serverRequest
+    self.apiEndpoint = ""
+
+  def initPayload(self, fileName):
+    try:
+      with open(fileName,"r+") as f:
+        self.payload = json.load(f)
+
+    except:
+      print "- File "+fileName+" could not be read" 
+      raise
+
+  def submit(self, customEndpoint=None):
+    if customEndpoint is None:
+      apiEndpoint = self.apiEndpoint
+    else:
+      apiEndpoint = customEndpoint
+
+    data = json.dumps(self.payload)
+    
+    return json.loads(self.serverRequest.submit(apiEndpoint,data=data))
+
+  def create(self):
+    # Create the object in Zoomdata and record the assigned ID
+    self.id = self.submit()["id"]
+
+  def delete(self):
+      # Delete the given object in Zoomdata
+      url = self.apiEndpoint+'/'+self.id
+      return self.serverRequest.submit(url,lmda='DELETE')
---
+++ api-examples/administration/load_zoomdata_security_model.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+from base_classes.zoomdata_api_base import ZoomdataRequest
+from base_classes.zoomdata_api_impala import ImpalaDatasource
+import sys,csv,json
+
+def main():
+  #load the Zoomdata instance info and credentials
+  with open('conf/zoomdata-server.json') as data_file:
+    zoomdataInfo = json.load(data_file)
+
+  #set local Zoomdata instance info
+  zoomdataBaseURL = zoomdataInfo["URL"]
+  adminUser = zoomdataInfo["AdminUserName"]
+  adminPassword = zoomdataInfo["AdminPassword"]
+  zoomdataAccountID= zoomdataInfo["zoomdataAccountID"]
+  ImpalaConnection = zoomdataInfo["ImpalaConnectionID"]
+  debug = zoomdataInfo["debugMode"]
+
+  #create the Zoomdata request object
+  zoomdataServerRequest = ZoomdataRequest(zoomdataBaseURL, adminUser, adminPassword)
+  # Enable verbose output if desired
+  if debug.lower() == "yes":
+    zoomdataServerRequest.enableDebug()
+
+  resetGroupList = []
+
+  #begin parsing the IDM Role configuration file; Its assumed this file has a column name header; Rows are processed one at a time
+  roleConfigFile = sys.argv[1]
+  rowCounter = 1
+
+  with open(roleConfigFile, 'rU') as csvfile:
+    print("-- Reading from "+roleConfigFile+" --")
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      print("----------------------------------------")
+      print("-- Processing row #"+str(rowCounter)+" --")
+      #retrieve a list of available data source names in Zoomdata
+      currentSources = json.loads(zoomdataServerRequest.submit(zoomdataBaseURL+'/service/sources?fields=name'))
+      #retrieve a list of available user groups in Zoomdata
+      currentGroups = json.loads(zoomdataServerRequest.submit(zoomdataBaseURL+'/service/groups?accountId='+zoomdataAccountID))
+
+      groupID = None
+      sourceID = None
+
+      schemaName = row['db']
+      tableName = row['table_name']
+      adGroup = row['group']
+      sourceName = row['zoomdata_source_name']
+      partitionField = row['time_partition_field']
+      partitionBaseField = row['time_partition_base_field'] #field that the partitionField is based on
+      #Look up the source by name in the currently configured source list
+      try:
+        sourceID = [x for x in currentSources if x["name"] == sourceName][0]['id']
+      except IndexError:
+        sourceID = None
+
+      #create the data source if doesn't already exist
+      if sourceID is None:
+        # Initialize the source object
+        newSource = ImpalaDatasource(sourceName, zoomdataServerRequest, ImpalaConnection, tableName, schemaName,partitionField=partitionField,partitionBaseField=partitionBaseField)
+        # Create the source in Zoomdata
+        newSource.create()
+        sourceID = newSource.id
+
+      #Look up the group by name in the currently configured group list
+      try:
+        groupID = [x for x in currentGroups if x["label"] == adGroup][0]['id']
+      except IndexError:
+        groupID = None
+
+      #create the AD group in Zoomdata if it doesn't already exist
+      if groupID is None:
+        data = '{"group":{"accountId":"'+zoomdataAccountID+'","configType":"manualAccess","label":"'+adGroup+'","description":"Auto Import AD Group","roles":["ROLE_SAVE_DASHBOARDS","ROLE_SHARE_DASHBOARDS","ROLE_SAVE_FILTERS","ROLE_EDIT_FORMULAS","ROLE_READ_FORMULAS","ROLE_CREATE_SOURCES"]},"bookmarks":[],"sources":[],"userIds":[]}'
+
+        try:
+          groupID = json.loads(zoomdataServerRequest.submit(zoomdataBaseURL+'/service/groups',data))['group']['id']
+          print("+ User Group "+adGroup+" successfully created")
+        except:
+            print("- User Group "+adGroup+" could not be created")
+            e = sys.exc_info()
+            print e
+      #retrieve group object from the server
+      groupObject = json.loads(zoomdataServerRequest.submit(zoomdataBaseURL+'/service/groups/'+groupID))
+
+      #[IMPORTANT] the next line will remove all currently accessible datasources from the group the first time the group is encourntered. The assumption here is that they will be added back according to what is configured in the mapping csv.
+      if groupID not in resetGroupList:
+        groupObject["sources"] = json.loads('[]')
+        resetGroupList.append(groupID)
+        print("+ Data Source security reset for "+adGroup)
+
+      #add source access to group object
+      groupObject["sources"].append(json.loads('{"id":"'+sourceID+'","admin":false,"create":false,"delete":false,"read":true,"write":false,"type":"SOURCE"}'))
+      #post updated group object back to server
+      zoomdataServerRequest.submit(zoomdataBaseURL+'/service/groups',json.dumps(groupObject))
+      print("+ Access to data source "+sourceName+" added for group "+adGroup)
+
+      rowCounter += 1
+main()
