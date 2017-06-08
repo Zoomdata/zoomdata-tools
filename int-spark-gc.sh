5b26e318b376c55cc29db4ab0e1c7d029c0952d3
Eugene Chepurniy <eugene@zoomdata.com>
2017-06-08 10:17:18 +0300

    fixed typo

--- int-spark-gc.sh
+++ int-spark-gc.sh
@@ -11,6 +11,6 @@ sleep 3
 echo "==> cleaning files older2 days in ${SPARK_TMP_DIR} directory"
 find "${SPARK_TMP_DIR}/" -mtime +2 -exec rm -r {} \;
 
-echo "==> Statring Zoomdata Application"
+echo "==> Starting Zoomdata Application"
 service zoomdata start
 
